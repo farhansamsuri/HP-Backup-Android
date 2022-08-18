e7d83fffd87ca22aef2e7494e7272076a49b83a0
Farhan <mdfarhansamsuri@gmail.com>
2022-08-18 23:50:50 +0800

    here you go suleen

--- App.js
+++ App.js
@@ -4,18 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import MainScreen from './src/screens/MainScreen';
 import TTS from './src/components/TTS';
-import SpellTypeScreen from './src/screens/SpellTypeScreen';
 import SpellList from './src/components/SpellList';
 import IndividualSpell from './src/components/IndividualSpell';
+import SpellTypes from './src/components/SpellTypes';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Welcome'>
-                <Stack.Screen name='TTS' component={TTS} screenOptions={{ headerShown: false }} />
-                <Stack.Screen name='Welcome' component={MainScreen} screenOptions={{ headerShown: false }} />
-                <Stack.Screen name='SpellTypes' component={SpellTypeScreen} options={{ headerShown: false }} />
+                {/* <Stack.Screen name='TTS' component={TTS} screenOptions={{ headerShown: false }} />
+                <Stack.Screen name='Welcome' component={MainScreen} screenOptions={{ headerShown: false }} /> */}
+                <Stack.Screen name='SpellTypes' component={SpellTypes} options={{ headerShown: false }} />
                 <Stack.Screen name='SpellList' component={SpellList} options={{ headerShown: false }} />
                 <Stack.Screen name='IndividualSpell' component={IndividualSpell} options={{ headerShown: false }} />
             </Stack.Navigator>
